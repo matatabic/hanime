09dd8718e0b90dbe36845eb57206a222bb46c64f
zzz62755 <q627556311@163.com>
2022-05-19 23:40:52 +0800

    xbg

--- lib/providers/download_state.dart
+++ lib/providers/download_state.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/foundation.dart';
+import 'package:hanime/entity/download_entity.dart';
 
 class Downloading {}
 
 class DownloadState with ChangeNotifier, DiagnosticableTreeMixin {
+  List<int> _downloadQueueList = [];
+  List<DownloadEntity> _downloadList = [];
+
+  List<int> get downloadQueueList => _downloadQueueList;
+  List<DownloadEntity> get downloadList => _downloadList;
+
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
